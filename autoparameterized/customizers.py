b48c80ddc187b7480deb1e00fed325d0afb7694c
legitimation <jeff426@naver.com>
2026-06-03 12:46:33 +0900

    customizers: implement chain customize

--- autoparameterized/customizers.py
+++ autoparameterized/customizers.py
@@ -99,3 +99,8 @@ class ChainCustomizer(Customizer):
                 raise TypeError("customizers must have a customize method")
 
         self.customizers = customizers
+
+    def customize(self, value):
+        for customizer in self.customizers:
+            value = customizer.customize(value)
+        return value
