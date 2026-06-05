34bc85c01f5e4f02e26a108a31f579017315b50c
legitimation <jeff426@naver.com>
2026-06-05 21:30:51 +0900

    customizers: split non-empty customizer

--- autoparameterized/customizers/__init__.py
+++ autoparameterized/customizers/__init__.py
@@ -2,31 +2,12 @@
 Reusable value customizers for generated test inputs.
 """
 
-from ..base import Customizer
 from .chain import ChainCustomizer
 from .length import LengthCustomizer
+from .non_empty import NonEmptyCustomizer
 from .regex_string import RegexStringCustomizer
 from .transform import TransformCustomizer
 from .type_cast import TypeCastCustomizer
-
-
-class NonEmptyCustomizer(Customizer):
-    """
-    Customizer that replaces empty strings/lists with defaults.
-    """
-
-    def __init__(self, default_string="value", default_item=None):
-        self.default_string = default_string
-        self.default_item = default_item
-
-    def customize(self, value):
-        if isinstance(value, str) and value == "":
-            return self.default_string
-
-        if isinstance(value, list) and len(value) == 0:
-            return [self.default_item]
-
-        return value
 
 
 __all__ = [
